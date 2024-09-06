ee0683ddbe107c23854f45a2416042706e021e62
jenniceangelj <jenniceangeljennytajxaysmg@hotmail.com>
2024-09-06 13:39:03 +0000

    Initialised

---
+++ contracts/NFT.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+contract TestNFT is ERC721, ERC721Burnable {
+    constructor()
+        ERC721("415140c53969ea44f74b56bc54cc4e242440b1f8bf5b170e1021f5de24e11444","415140c53969ea44f74b56bc54cc4e242440b1f8bf5b170e1021f5de24e11444")
+    {}
+
+    function safeMint(address to, uint256 tokenId) public {
+        _safeMint(to, tokenId);
+    }
+}
